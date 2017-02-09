7f852eb4b85f11c5908a99dde8afc453ae94f238
Lyman Hurd <lyman.hurd@gmail.com>
2017-02-08 22:14:25 -0800

    Numerous fixes.

--- tree_search.py
+++ tree_search.py
@@ -9,7 +9,7 @@ __author__ = 'lhurd'
 
 NEGATIVE_INFINITY = -99999
 INFINITY = 99999
-INITIAL_DEPTH = 3
+INITIAL_DEPTH = 5
 
 
 def find_move(board, is_black):
@@ -38,11 +38,12 @@ def find_move(board, is_black):
     # If there are ties, choose randomly from among the tied choices.
     if best_children:
         best_child = best_children[random.randint(0, len(best_children) - 1)]
+        # best_child = best_children[0]
+
     if best_child and not is_black:
         result = flip(best_child)
     else:
         result = best_child
-    logging.debug('final eval %d bd %s' % (best_value, result))
     return result
 
 
@@ -64,28 +65,23 @@ def negamax(board, depth, alpha, beta):
 
     # We do not stop the search if there are pending captures.
     if depth <= 0 and not has_jump(board):
-        value = evaluate(board)
-        if depth % 2:
-            logging.debug('leaf depth %d eval %d bd %s' % (depth, -value,
-                                                           flip(board)))
-        else:
-            logging.debug(
-                'leaf depth %d eval %d bd %s' % (depth, value, board))
-        return value
+        return evaluate(board)
     best_value = NEGATIVE_INFINITY
-    children = sorted(find_children(board), key=evaluate, reverse=True)
+    children = find_children(flip(board))
     for child in children:
-        value = - negamax(flip(child), depth - 1, -beta, -alpha)
-        logging.debug('node depth %d eval %d' % (depth, (-1) ** depth * value))
+        value = negamax(child, depth - 1, -beta, -alpha)
         best_value = max(best_value, value)
         alpha = max(alpha, value)
         if alpha > beta:
             break
-    return best_value
+    return -best_value
 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
+    # find_move('-bbbbbbb-bbbbb-----rrrrrrrrrrrr-', False)
+    # print negamax('-bbbbbbb-bbbbb----rrrrr-rrrrrrr-', INITIAL_DEPTH,
+    # NEGATIVE_INFINITY, INFINITY)
     bd = STARTING_BOARD
     black = True
     i = 0
